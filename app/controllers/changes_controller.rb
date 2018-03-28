58cb92f1ef1405ef9290683b45c5a4dc5535f30e
Scott Robertson <scottymeuk@gmail.com>
2018-03-28 09:06:12 +0100

    Add region filtering
    #1

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -2,6 +2,7 @@ class ChangesController < ApplicationController
   def index
     @changes = JSON.parse(RestClient.get('https://supercharge.info/service/supercharge/allChanges'))
     @changes = @changes.select{|d| d['country'].downcase == params[:country].downcase } if params[:country].present?
+    @changes = @changes.select{|d| d['region'].downcase == params[:region].downcase } if params[:region].present?
 
     respond_to do |format|
       format.rss { render :layout => false }
